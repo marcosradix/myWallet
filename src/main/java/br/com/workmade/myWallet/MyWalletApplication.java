f347ae66da5b4b0f34476139f51bf7aa4e2bfa57
MARCOS FERREIRA SEVERINO <008974CX@sefaz-ce.gov.br>
2020-02-12 16:32:48 -0300

    first commit

---
+++ src/main/java/br/com/workmade/myWallet/MyWalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.workmade.myWallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/workmade/myWallet/MyWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.workmade.myWallet;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
